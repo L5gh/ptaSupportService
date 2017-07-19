7b2628ff4ccbd1b29ab3feb507f2808494860820
Reiko Yamada <yamada@L5mbp.local>
2017-07-19 14:52:31 +0900

    １〜６年生をON/OFFした時の全学年の処理

--- src/app/components/first/sendContact.jsx
+++ src/app/components/first/sendContact.jsx
@@ -7,17 +7,16 @@ class sendContact extends Component {
     super(props);
     this.dataChangeSelection = this.dataChangeSelection.bind(this);
     this.state = {
-      data: [],
+      grade     : [],
+      All       : [],
+      checkCount: 0
     };
+    this.count = 0;
   }
 
   componentDidMount = () => {
     this.setState({
-      data: [
-        { index   : '13',
-          label   : '全学年',
-          hl      : '',
-          selected: false },
+      grade: [
         { index   : '1',
           label   : '1年生',
           hl      : 'low' ,
@@ -49,12 +48,18 @@ class sendContact extends Component {
           selected: false,
         }
       ],
+      All: [
+        { index   : '10',
+          label   : '全学年',
+          hl      : '',
+          selected: false }
+      ]
     });
   }
 
   // チェックボッスクがON/OFFされた場合の処理
   dataChangeSelection = (targetId) => {
-    const nextdata = this.state.data.map((d) => {
+    const checkAll = this.state.All.map(d => {
       if (d.index === targetId.currentTarget.id) {
         return {
           index   : d.index,
@@ -62,6 +67,24 @@ class sendContact extends Component {
           hl      : d.hl,
           selected: !d.selected
         };
+      }else{
+        return{
+          index   : d.index,
+          label   : d.label,
+          hl      : d.hl,
+          selected: d.selected
+        };
+      }
+    });
+    const checkGrade = this.state.grade.map((d) => {
+      if (d.index === targetId.currentTarget.id) {
+        return {
+          index   : d.index,
+          label   : d.label,
+          hl      : d.hl,
+          selected: !d.selected
+        };
+        //全学年がON/OFFされた場合の処理
       } else if (targetId.currentTarget.value === '') {
         return {
           index   : d.index,
@@ -78,33 +101,53 @@ class sendContact extends Component {
         };
       }
     });
-    this.setState({data: nextdata});
-
     //1~6年生全てがチェックされた時
-  //   const chkAll = nextdata.map((all) => {
-  //     if (targetId.currentTarget.hl === '') {
-  //       for (let i = 0; i <= all.lengh; i ++) {
-  //         return {
-  //           index   : all.index,
-  //           label   : all.label,
-  //           hl      : all.hl,
-  //           selected: true
-  //         };
-  //       }
-  //     } else {
-  //       return {
-  //         index   : all.index,
-  //         label   : all.label,
-  //         hl      : all.hl,
-  //         selected: all.selected
-  //       };
-  //     }
-  //   });
-  //   this.setState({data: chkAll});
+    this.count = 0;
+    checkGrade.map(data => {
+      if(data.selected === true) {
+        this.count = this.count + 1;
+      }
+    });
+    console.log('count:'+this.count);
+
+    const AllData = this.state.All.map(d => {
+      if (this.count === 6) {
+        return {
+          index   : d.index,
+          label   : d.label,
+          hl      : d.hl,
+          selected: true
+        };
+      }else{
+        return{
+          index   : d.index,
+          label   : d.label,
+          hl      : d.hl,
+          selected: false
+        };
+      }
+    });
+
+    this.setState({grade: checkGrade});
+    this.setState({All: checkAll});
+    this.setState({All: AllData});
   };
 
   render() {
-    const chkBoxData = this.state.data.map((data) => {
+    const nextAll = this.state.All.map((data) => {
+      return (
+        <div key={ data.index } className="checkbox-inline">
+          <input
+            id={data.index}
+            type="checkbox"
+            onChange={this.dataChangeSelection.bind(this)}
+            checked={data.selected}
+            value={data.hl}
+          >{data.label}</input>
+        </div>
+      );
+    });
+    const nextGrade = this.state.grade.map((data) => {
       return (
         <div key={ data.index } className="checkbox-inline">
           <input
@@ -142,7 +185,8 @@ class sendContact extends Component {
           </div>
           <div className="col-xs-12"></div>
           <div className="form-group col-xs-12">
-            {chkBoxData}
+            {nextAll}
+            {nextGrade}
           </div>
           <div className="col-xs-12"></div>
           <div className="col-xs-4"></div>
